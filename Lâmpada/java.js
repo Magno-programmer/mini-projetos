d036e92c492a3e360c58936a582b07752c45108a
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2021-11-10 00:49:11 -0300

    Update java.js

--- Lâmpada/java.js
+++ Lâmpada/java.js
@@ -2,25 +2,30 @@ const lampada = document.getElementById('lampada');
 const ligar = document.getElementById('ligar');
 const desligar = document.getElementById('desligar');
 
+function continuaQuebrada(){
+    return lampada.src.indexOf('quebrada') > -1;
+}
+
 function on(){
-    lampada.src = './img/ligada.jpg';
+    if(!continuaQuebrada()){
+        lampada.src = './img/ligada.jpg';
+    }
 }
 
 function off(){
-    lampada.src = './img/desligada.jpg';
+    if(!continuaQuebrada()){
+        lampada.src = './img/desligada.jpg';
+    }
+    
 }
 
-function turnOff(){
-    lampada.src = './img/desligada.jpg';
-}
-
-
-function turnOn(){
-    lampada.src = './img/ligada.jpg';
+function quebra(){
+    lampada.src = './img/quebrada.jpg'
 }
 
 
 ligar.addEventListener('click', on);
 desligar.addEventListener('click', off);
-lampada.addEventListener('mouseleave', turnOff);
-lampada.addEventListener('mouseenter', turnOn);
+lampada.addEventListener('mouseleave', off);
+lampada.addEventListener('mouseenter', on);
+lampada.addEventListener('dblclick', quebra)
